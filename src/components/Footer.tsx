183615cb2dd10f9a300d2cb8898c852221096927
Jie Chen <jiechenmc@gmail.com>
2022-11-10 17:30:49 -0500

    added simple styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <>
-      <footer>Words</footer>
+      <footer class="absolute bottom-0 left-0 right-0 text-center text-blue-800 my-1">
+        &copy;Loqi 2022
+      </footer>
     </>
   );
 };
