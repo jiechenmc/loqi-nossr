dc96169872ae61e9e8a3333171c923610940ef58
Jie Chen <jiechenmc@gmail.com>
2022-11-10 10:48:34 -0500

    before fixing tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,44 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import Message from "./components/Message";
+import { useEffect, useState } from "preact/hooks";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
 
-export function App() {
-  const [count, setCount] = useState(0)
+const firebaseConfig = {
+  apiKey: "AIzaSyA2ffbETwL8hupSveo6d55YTOun0kYzCC4",
+  authDomain: "loqi-loqi.firebaseapp.com",
+  databaseURL: "https://loqi-loqi-default-rtdb.firebaseio.com",
+  projectId: "loqi-loqi",
+  storageBucket: "loqi-loqi.appspot.com",
+  messagingSenderId: "622541820845",
+  appId: "1:622541820845:web:e7b417188f959ea1c40724",
+  measurementId: "G-HZDJC6PYH8",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export default function App() {
+  const [msgs, setMsgs] = useState<any>();
+
+  const messagesRef = ref(
+    database,
+    `data/rooms/Stony Brook University/CSE214/messages`
+  );
+
+  useEffect(() => {
+    onValue(messagesRef, (snapshot: any) => {
+      const data = snapshot.val();
+      if (data != null) {
+        const vals = Object.values(data);
+        const dt = vals.map((e: any) => <Message text={e.content} />);
+        setMsgs(dt);
+      }
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+    <div class="flex gap-2 w-full">
+      <p class="flex-grow-1 font-bold text-xl">{msgs}</p>
+    </div>
+  );
 }
